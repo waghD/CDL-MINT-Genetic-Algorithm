149e01b83543b274302404c2a53ce7431fc0c86b
waghD <33834105+waghD@users.noreply.github.com>
2020-07-23 10:07:10 +0200

    Finish implementation and logging
    Multiple test run loop
    1 chromosome per sensor with 2 genes each
    gaussian mutator
    single point crossover
    cleaned imports and unused methods from main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,50 +2,28 @@ package main;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.math.RoundingMode;
-import java.sql.Timestamp;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import static java.lang.Math.PI;
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
-import static io.jenetics.engine.EvolutionResult.toBestPhenotype;
-import static io.jenetics.engine.Limits.bySteadyFitness;
 
 import io.jenetics.Chromosome;
 import io.jenetics.DoubleChromosome;
 import io.jenetics.DoubleGene;
+import io.jenetics.GaussianMutator;
 import io.jenetics.Genotype;
-import io.jenetics.MeanAlterer;
-import io.jenetics.Mutator;
 import io.jenetics.Optimize;
 import io.jenetics.Phenotype;
-import io.jenetics.RouletteWheelSelector;
+import io.jenetics.SinglePointCrossover;
 import io.jenetics.TournamentSelector;
-import io.jenetics.engine.Codecs;
 import io.jenetics.engine.Engine;
 import io.jenetics.engine.EvolutionResult;
 import io.jenetics.engine.EvolutionStatistics;
-import io.jenetics.stat.MinMax;
-import io.jenetics.util.DoubleRange;
-//import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 import io.jenetics.util.ISeq;
-import harmony.HarmonyMemory;
-import harmony.HarmonyParameters;
-import harmony.HarmonyResult;
-import harmony.HarmonySearch;
 
-import java.util.HashMap;
-import output.Printer;
 import timeSeries.TimeSeriesDatabase;
 
 public class Main {
@@ -54,22 +32,33 @@ public class Main {
 	 * private static final String OUTPUT_DIRECTORY = "../harmonyresult"; private
 	 * static final String TEST_NAME = "test_refactor";
 	 */
+
+	// !Achtung reihenfolge muss mit getSensorOffsetMap zusammen stimmen!
 	private static final AxisStream[] axisArr = { AxisStream.GP, AxisStream.MAP, AxisStream.BP, AxisStream.SAP,
 			AxisStream.WP };
 	private static List<AxisStream> axisList = null;
 	private static final List<String> statesToNotEvaluateList = new ArrayList<String>();
-	
+
 	// Test Output
 	private static final boolean OUTPUT_TO_FILE = true;
 	private static final String OUTPUT_DIRECTORY = "../genetic_result";
-	private static final String TEST_NAME = "test_output";
+	private static final String TEST_NAME = "output_test";
+	private static final int TEST_COUNT = 5;
 
 	// The fitness function.
-	private static double fitness(final Genotype<DoubleGene> x) {
+	private static double fitness(final Genotype<DoubleGene> genotype) {
 
 		// Allele = Jenetic's wording for the actual gene values that
 		// are stored in a chromosome (in our case of type double)
-		double[] alleles = x.chromosome().as(DoubleChromosome.class).toArray();
+		double[] alleles = new double[genotype.geneCount()];
+
+		int i = 0;
+		for (Chromosome<DoubleGene> chromosome : genotype) {
+			for (DoubleGene gene : chromosome) {
+				alleles[i] = gene.doubleValue();
+				i++;
+			}
+		}
 
 		Map<String, PropertyBoundaries> propertyMap = getSensorOffsetMap(alleles);
 
@@ -94,121 +83,125 @@ public class Main {
 
 		eval.setUpRealDataStream(axisList);
 
-		PrintStream outputFile = null;
-		PrintStream resultFile = null;
-		
-		// Create output directory
-		if(OUTPUT_TO_FILE) {
-			String outputDir = OUTPUT_DIRECTORY + "/" + TEST_NAME;
-			new File(outputDir).mkdirs();
-			
-			String fileBase = outputDir + "/" + TEST_NAME;
-			try { 
-				outputFile = new PrintStream(new FileOutputStream(fileBase + "_logs" + ".csv", true), true); 
-				resultFile = new PrintStream(new FileOutputStream(fileBase + "_result" + ".txt", true), true);
-				System.setOut(outputFile); 
-			} catch (IOException e) { 
-				System.err.print(e.getMessage());
-				e.printStackTrace();
-			}
-		}
-
 		// final Factory<Genotype<DoubleGene>> gtf = Genotype.of(DoubleChromosome.of())
 
 		final Engine<DoubleGene, Double> engine = Engine
 				// Create a new builder with the given fitness
 				// function and chromosome.
-				.builder(Main::fitness, DoubleChromosome.of(0.0, 1.0, nrOfSensors * 2))
+				.builder(Main::fitness, Genotype.of(DoubleChromosome.of(0.0, 1.0, 2), nrOfSensors))
 				.offspringFraction(0.7)
-				.survivorsSelector(new RouletteWheelSelector<>())
-				.offspringSelector(new TournamentSelector<>())
+				.survivorsSelector(new TournamentSelector<>(5))
+				.offspringSelector(new TournamentSelector<>(5))
 				.populationSize(100)
 				.optimize(Optimize.MAXIMUM)
-				.alterers(new Mutator<>(0.1), new MeanAlterer<>(0.8))
-				// Build an evolution engine with the
-				// defined parameters.
+				.alterers(new GaussianMutator<>(0.1), new SinglePointCrossover<>(0.8))
 				.build();
 
-		// Create evolution statistics consumer.
-		final EvolutionStatistics<Double, ?> statistics = EvolutionStatistics.ofNumber();
-		
-		StringBuilder headerStr = new StringBuilder();
-		for(int i = 0; i < nrOfSensors; i++) {
-			headerStr.append("Sensor ");
-			headerStr.append(i);
-			headerStr.append(" Lower;");
-			headerStr.append("Sensor ");
-			headerStr.append(i);
-			headerStr.append(" Upper;");
-		}
-		
-		headerStr.append("F-Measure");
-		
-		System.out.println(headerStr);
+		// Exec algorithm multiple times for test
+		for (int iteration = 0; iteration < TEST_COUNT; iteration++) {
 
-		final ISeq<EvolutionResult<DoubleGene, Double>> best = engine.stream()
-				// Truncate the evolution stream after 7 "steady"
-				// generations.
-				// .limit(bySteadyFitness(5))
-				// The evolution will stop after maximal 100
-				// generations.
-				.limit(1000)
-				// Update the evaluation statistics after
-				// each generation
-				.peek(statistics)
-				.peek((evolutionResult) -> {
-					Phenotype<DoubleGene, Double> bestPheno = evolutionResult.bestPhenotype();
-					Genotype<DoubleGene> genum = bestPheno.genotype();
-					StringBuilder str = new StringBuilder();
-					for(Chromosome<DoubleGene> chromosome : genum) {
-						for(DoubleGene gene : chromosome) {
-							str.append(gene.doubleValue());
-							str.append(";");
-						}
-					}
-					str.append(bestPheno.fitness());
-					System.out.println(str);
-				})
-				// Collect (reduce) the evolution stream to
-				// its best phenotype.
-				// .collect(EvolutionResult.toBestEvolutionResult());
-				// .flatMap(MinMax.toStrictlyDecreasing())
-				.collect(ISeq.toISeq(1000));
+			PrintStream outputFile = null;
+			PrintStream resultFile = null;
 
-		// .collect(ISeq.toISeq(10));
-		
-		if(OUTPUT_TO_FILE) {
-			System.setOut(resultFile); 
-		}
-		
-		System.out.println(statistics);
-		ArrayList<EvolutionResult<DoubleGene, Double>> arrayList = new ArrayList<>(best.asList());
-		double fittestVal = 0.0;
-		for (int i = 0; i < arrayList.size(); i++) {
-			EvolutionResult<DoubleGene, Double> curResult = arrayList.get(i);
-			List<Phenotype<DoubleGene, Double>> solutions = curResult.population().asList();
-			for (int j = 0; j < solutions.size(); j++) {
-				Phenotype<DoubleGene, Double> curSol = solutions.get(j);
-				if (curSol.fitness() > fittestVal) {
-					System.out.println(i + ":");
-					System.out.println(curSol);
-					fittestVal = curSol.fitness();
+			// Create output directory
+			if (OUTPUT_TO_FILE) {
+				String outputDir = OUTPUT_DIRECTORY + "/" + TEST_NAME;
+				new File(outputDir).mkdirs();
+
+				String fileBase = outputDir + "/" + TEST_NAME;
+				try {
+					outputFile = new PrintStream(
+							new FileOutputStream(fileBase + "_" + iteration + "_logs" + ".csv", true), true);
+					resultFile = new PrintStream(
+							new FileOutputStream(fileBase + "_" + iteration + "_result" + ".txt", true), true);
+					System.setOut(outputFile);
+				} catch (IOException e) {
+					System.err.print(e.getMessage());
+					e.printStackTrace();
 				}
-
 			}
-		}
-		System.out.println(fittestVal);
 
-		/*
-		 * for(EvolutionResult<DoubleGene, Double> result: best) {
-		 * System.out.println(result.population());
-		 * System.out.println(result.population().size()); }
-		 */
-		if(outputFile != null) {
-			outputFile.close();
-		}
-		if(resultFile != null) {
-			outputFile.close();
+			// Create evolution statistics consumer.
+			final EvolutionStatistics<Double, ?> statistics = EvolutionStatistics.ofNumber();
+
+			StringBuilder headerStr = new StringBuilder();
+			for (int i = 0; i < axisArr.length; i++) {
+
+				headerStr.append(axisArr[i].getAxisName() + " ");
+				headerStr.append(" lower;");
+				headerStr.append(axisArr[i].getAxisName() + " ");
+				headerStr.append(" upper;");
+			}
+
+			headerStr.append("F-Measure");
+
+			System.out.println(headerStr);
+
+			final ISeq<EvolutionResult<DoubleGene, Double>> best = engine.stream()
+					// Truncate the evolution stream after 7 "steady"
+					// generations.
+					// .limit(bySteadyFitness(5))
+					// The evolution will stop after maximal 1000
+					// generations.
+					.limit(1000)
+					// Update the evaluation statistics after
+					// each generation
+					.peek(statistics)
+					// print generation log
+					.peek((evolutionResult) -> {
+						Phenotype<DoubleGene, Double> bestPheno = evolutionResult.bestPhenotype();
+						Genotype<DoubleGene> genum = bestPheno.genotype();
+						StringBuilder str = new StringBuilder();
+						for (Chromosome<DoubleGene> chromosome : genum) {
+							for (DoubleGene gene : chromosome) {
+								str.append(gene.doubleValue());
+								str.append(";");
+							}
+						}
+						str.append(bestPheno.fitness());
+						System.out.println(str);
+					})
+					// Collect (reduce) the evolution stream to
+					// its best phenotype.
+					// .collect(EvolutionResult.toBestEvolutionResult());
+					// .flatMap(MinMax.toStrictlyDecreasing())
+					.collect(ISeq.toISeq(1000));
+
+			// .collect(ISeq.toISeq(10));
+
+			if (OUTPUT_TO_FILE) {
+				System.setOut(resultFile);
+			}
+
+			System.out.println(statistics);
+			ArrayList<EvolutionResult<DoubleGene, Double>> arrayList = new ArrayList<>(best.asList());
+			double fittestVal = 0.0;
+			for (int i = 0; i < arrayList.size(); i++) {
+				EvolutionResult<DoubleGene, Double> curResult = arrayList.get(i);
+				List<Phenotype<DoubleGene, Double>> solutions = curResult.population().asList();
+				for (int j = 0; j < solutions.size(); j++) {
+					Phenotype<DoubleGene, Double> curSol = solutions.get(j);
+					if (curSol.fitness() > fittestVal) {
+						System.out.println(i + ":");
+						System.out.println(curSol);
+						fittestVal = curSol.fitness();
+					}
+
+				}
+			}
+			System.out.println(fittestVal);
+
+			/*
+			 * for(EvolutionResult<DoubleGene, Double> result: best) {
+			 * System.out.println(result.population());
+			 * System.out.println(result.population().size()); }
+			 */
+			if (outputFile != null) {
+				outputFile.close();
+			}
+			if (resultFile != null) {
+				outputFile.close();
+			}
 		}
 	}
 
@@ -248,35 +241,6 @@ public class Main {
 
 		}
 		return sensorOffsetMap;
-	}
-
-	/**
-	 * Execute harmony search with given parameters and prints some information
-	 * (influenced by constants in main).
-	 * 
-	 * @param hpa .. Configuration Object of Harmony Search
-	 * 
-	 * @return HarmonyResult
-	 */
-	static HarmonyResult runHarmonySearch(HarmonyParameters hpa) {
-		// Initialize HarmonyMemory with HarmonyParameters
-		HarmonyMemory memory = new HarmonyMemory(hpa);
-
-		// Initialize Harmony Search with Harmony Memory and Parameters
-		HarmonySearch search = new HarmonySearch(memory, hpa);
-
-		// Execute harmony search: If parameter "stopIfOptimumFound" is true,
-		// max number of iterations is still respected (here: max 300 iterations)
-		HarmonyResult hs = search.execHarmonySearch();
-
-		// Print results of this Harmony Search run
-		Printer.printHeader("HARMONY RESULTS");
-
-		System.out.println(hs);
-		Printer.printHeader("BEST");
-		memory.print(memory.findBestEvalResult());
-
-		return hs;
 	}
 
 	private static void setUpDatabase(String filenameData, boolean longRun, int timespan) {
